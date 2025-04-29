41ac71ba696813a5b5022a2e34ccd4b96b6c2c43
Tommy Day <day@meetmaestro.com>
2025-04-28 20:51:07 -0400

    Still Be Fixin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 import node from '@astrojs/node';
 import clerk from '@clerk/astro';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-    port: process.env.PORT || 4321,    // <--- ADD THIS
-    host: '0.0.0.0'                    // <--- ADD THIS
+  adapter: node({ 
+    mode: 'standalone', 
+    port: process.env.PORT || 4321, 
+    host: '0.0.0.0' 
   }),
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
     clerk(),
+    tailwind({ applyBaseStyles: false }),
+    react(),
   ],
 });
